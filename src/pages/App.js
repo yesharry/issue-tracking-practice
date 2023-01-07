d5e8aff9361b4c26b229bd3605c4b9f857b5f8a9
yesharry <reerohh@gmail.com>
2023-01-07 17:41:29 +0900

    remove: 파일 초기화

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,21 +1,7 @@
-import Header from '../components/app/Header';
-import Body from '../components/app/Body';
 import styled from 'styled-components';
 
 function App() {
-  return (
-    <Wrapper>
-      <Header />
-      <Body />
-    </Wrapper>
-  );
+  return <>ㅇㅇㅇ</>;
 }
-
-const Wrapper = styled.div`
-  width: 1250px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
 export default App;
